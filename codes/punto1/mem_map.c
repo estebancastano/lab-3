aaa84d5a0bc8c9166547bef941091b9a93efc711
Jorge Luis <56008559+joluro86@users.noreply.github.com>
2026-05-07 14:39:48 -0500

    Add files via upload

---
+++ codes/punto1/mem_map.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int global_var = 42;
+
+int main() {
+    int local_var = 10;
+    int *heap_var = malloc(sizeof(int)*100);
+    *heap_var = 99;
+
+    printf("PID del proceso    : %d\n", getpid());
+    printf("Dir. codigo (main) : %p\n", (void*)main);
+    printf("Dir. global_var    : %p\n", (void*)&global_var);
+    printf("Dir. local_var     : %p\n", (void*)&local_var);
+    printf("Dir. heap_var      : %p\n", (void*)heap_var);
+
+    printf("\nPresione ENTER para continuar...\n");
+    getchar();
+    free(heap_var);
+    return 0;
+}
